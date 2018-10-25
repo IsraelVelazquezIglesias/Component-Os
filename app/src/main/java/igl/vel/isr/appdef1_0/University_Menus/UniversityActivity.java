3c0e60b9151d09e68f16540836b82fa060875b13
IsraelVelazquezIglesias <israelvelazqueziglesias@users.noreply.github.com>
2018-10-25 15:46:19 -0500

    Creacion de Actividad de University para seleccion de universidades y su respectivo archivo XML aun en blanco

---
+++ app/src/main/java/igl/vel/isr/appdef1_0/University_Menus/UniversityActivity.java
@@ -0,0 +1,16 @@
+package igl.vel.isr.appdef1_0.University_Menus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import igl.vel.isr.appdef1_0.R;
+
+public class UniversityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_university);
+    }
+
+}
