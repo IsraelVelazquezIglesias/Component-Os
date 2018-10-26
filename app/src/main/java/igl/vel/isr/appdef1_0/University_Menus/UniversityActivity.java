ca69c8430fe145e54cd3051aeeb19e23e2d42d58
IsraelVelazquezIglesias <israelvelazqueziglesias@users.noreply.github.com>
2018-10-25 20:13:53 -0500

    Actualizacion de University Activity agregando 2 escuelas V1.0

--- app/src/main/java/igl/vel/isr/appdef1_0/University_Menus/UniversityActivity.java
+++ app/src/main/java/igl/vel/isr/appdef1_0/University_Menus/UniversityActivity.java
@@ -1,14 +1,17 @@
 package igl.vel.isr.appdef1_0.University_Menus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import java.util.ArrayList;
 
 import igl.vel.isr.appdef1_0.Adaptadores.AdapterRecycler;
+import igl.vel.isr.appdef1_0.MainActivity;
 import igl.vel.isr.appdef1_0.R;
 import igl.vel.isr.appdef1_0.var_per.stringblob;
 
@@ -17,17 +20,29 @@ public class UniversityActivity extends AppCompatActivity {
     private RecyclerView seleccion;
     private ArrayList<stringblob> listEscuelas;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_university);
 
-        seleccion = (RecyclerView) findViewById(R.id.SeleccionUniversidad);
+        set_toolbar();
+        set_recycler();
+    }
 
+    private void set_toolbar() {
+
+        Toolbar toolbar = findViewById(R.id.toolUni);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Universidades");
+    }
+
+    private void set_recycler() {
+        seleccion = (RecyclerView) findViewById(R.id.SeleccionUniversidad);
         seleccion.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         listEscuelas = new ArrayList<>();
         llenarEscuelas();
-        AdapterRecycler adapterRecycler = new AdapterRecycler(listEscuelas);
+        AdapterRecycler adapterRecycler = new AdapterRecycler(listEscuelas, 2);
         adapterRecycler.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,7 +50,6 @@ public class UniversityActivity extends AppCompatActivity {
             }
         });
         seleccion.setAdapter(adapterRecycler);
-
     }
 
     private void llenarEscuelas() {
@@ -45,6 +59,31 @@ public class UniversityActivity extends AppCompatActivity {
     }
 
     private void clickOnCard(View v) {
+
+        String descripcion = listEscuelas.get(seleccion.getChildAdapterPosition(v)).getDesc();
+        switch (descripcion) {
+
+            case "Escuela Superior de Ingeniería Mecánica y Eléctrica": {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            break;
+
+            case "Escuela Superior de Ingeniería Química e Industrias Extractivas": {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            break;
+
+            default: {
+
+            }
+            break;
+
+        }
+
     }
 
 }
